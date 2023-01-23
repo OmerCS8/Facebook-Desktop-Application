a83bf20b984769d5b2b9a40aa8a26ae88e38b375
Omer Cohen Shor <omercs8@gmail.com>
2023-01-23 21:36:07 +0200

    adding decorator and named

---
+++ A23 Ex03 OmerCohenShor 207423906 RavidYael 206782666/FacebookAppWinformsUI/FormsUtils/PictureBoxDecorator.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicFacebookFeatures.FormsUtils
+{
+    class PictureBoxDecorator : PictureBox
+    {
+        protected PictureBox m_InnerPictureBox;
+
+        protected PictureBoxDecorator(PictureBox i_InnerPictureBox): this()
+        {
+            InnerPictureBox = i_InnerPictureBox;
+        }
+
+        protected PictureBoxDecorator()
+        {
+            this.ControlAdded += this.pictureBox_ControlAdded;
+            addVisualEffects();
+        }
+
+        public new Size Size
+        {
+            get => base.Size;
+            set
+            {
+                int newInnerWidth = Math.Max(m_InnerPictureBox.Size.Width + (value.Width - this.Size.Width), 0);
+                int newInnerHeight = Math.Max(m_InnerPictureBox.Size.Height + (value.Width - this.Size.Height), 0);
+                base.Size = value;
+                m_InnerPictureBox.Size = new Size(newInnerWidth, newInnerHeight);
+            }
+        }
+        
+        public PictureBox InnerPictureBox
+        {
+            get => m_InnerPictureBox;
+            set
+            {
+                if(m_InnerPictureBox != null)
+                {
+                    this.Controls.Remove(m_InnerPictureBox);
+                }
+
+                m_InnerPictureBox = value;
+                if(value != null)
+                {
+                    this.Controls.Add(m_InnerPictureBox);
+                    m_InnerPictureBox.Location = this.Location;
+                    m_InnerPictureBox.Size = this.Size;
+                }
+            }
+        }
+
+        private void pictureBox_ControlAdded(object i_Sender, ControlEventArgs i_E)
+        {
+            if(i_Sender is Control addedControl)
+            {
+                addedControl.MouseDown += (i_NewSender, i_Args) => this.OnMouseDown(i_Args);
+                addedControl.MouseUp += (i_NewSender, i_Args) => this.OnMouseUp(i_Args);
+                addedControl.MouseEnter += (i_NewSender, i_Args) => this.OnMouseEnter(i_Args);
+                addedControl.MouseLeave += (i_NewSender, i_Args) => this.OnMouseLeave(i_Args);
+                addedControl.Click += (i_NewSender, i_Args) => this.OnClick(i_Args);
+            }
+        }
+
+        private void pictureBox_MouseLeave(object i_Sender, EventArgs i_E)
+        {
+            this.BackColor = ColorsUtils.sr_PictureBoxBorderDefault;
+        }
+
+        private void pictureBox_MouseEnterAndUp(object i_Sender, EventArgs i_E)
+        {
+            this.BackColor = ColorsUtils.sr_PictureBoxBorderHover;
+        }
+
+        private void pictureBox_MouseDown(object i_Sender, EventArgs i_E)
+        {
+            this.BackColor = ColorsUtils.sr_PictureBoxBorderChosen;
+        }
+
+        private void addVisualEffects()
+        {
+            this.BackColor = ColorsUtils.sr_PictureBoxBorderDefault;
+            this.Cursor = Cursors.Hand;
+            this.MouseDown += this.pictureBox_MouseDown;
+            this.MouseUp += this.pictureBox_MouseEnterAndUp;
+            this.MouseEnter += this.pictureBox_MouseEnterAndUp;
+            this.MouseLeave += this.pictureBox_MouseLeave;
+        }
+    }
+}
---
+++ A23 Ex03 OmerCohenShor 207423906 RavidYael 206782666/FacebookAppWinformsUI/FormsUtils/PictureBoxNamed.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicFacebookFeatures.FormsUtils
+{
+    class PictureBoxNamed : PictureBoxDecorator
+    {
+        private readonly Label r_PictureNameLabel = new Label() { AutoEllipsis = true, Visible = true };
+        private const string k_DefaultPictureName = "";
+
+        public PictureBoxNamed(PictureBox i_InnerPictureBox = null ,string i_PictureName = k_DefaultPictureName) : base(i_InnerPictureBox)
+        {
+            (this as PictureBox).Size = new Size(this.Size.Width, this.Size.Height + 10);
+            this.m_InnerPictureBox.Top = 10;
+            this.Controls.Add(r_PictureNameLabel);
+        }
+
+        public string PictureName
+        {
+            get => r_PictureNameLabel.Text;
+            set => r_PictureNameLabel.Text = value;
+        }
+    }
+}
